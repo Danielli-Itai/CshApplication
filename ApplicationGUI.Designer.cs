69e9021a0e1ca94a76d82d0577c0b635b691c001
Itai Danielli <Danielli.Itai@gmail.com>
2022-05-07 22:07:07 +0300

    Legal notice. Addition command.

--- ApplicationGUI.Designer.cs
+++ ApplicationGUI.Designer.cs
@@ -35,40 +35,45 @@ namespace CshApplication
 			this.ResultText = new System.Windows.Forms.RichTextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
+			this.Add = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// Multiply
 			// 
-			this.Multiply.Location = new System.Drawing.Point(100, 101);
+			this.Multiply.Location = new System.Drawing.Point(11, 90);
+			this.Multiply.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Multiply.Name = "Multiply";
-			this.Multiply.Size = new System.Drawing.Size(148, 29);
+			this.Multiply.Size = new System.Drawing.Size(71, 24);
 			this.Multiply.TabIndex = 0;
-			this.Multiply.Text = "Multiply";
+			this.Multiply.Text = "*";
 			this.Multiply.UseVisualStyleBackColor = true;
 			this.Multiply.Click += new System.EventHandler(this.Multiply_Click);
 			// 
 			// Number2Text
 			// 
-			this.Number2Text.Location = new System.Drawing.Point(199, 42);
+			this.Number2Text.Location = new System.Drawing.Point(149, 34);
+			this.Number2Text.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Number2Text.Name = "Number2Text";
-			this.Number2Text.Size = new System.Drawing.Size(106, 25);
+			this.Number2Text.Size = new System.Drawing.Size(80, 21);
 			this.Number2Text.TabIndex = 3;
 			this.Number2Text.Text = "";
 			// 
 			// Number1Text
 			// 
-			this.Number1Text.Location = new System.Drawing.Point(33, 42);
+			this.Number1Text.Location = new System.Drawing.Point(25, 34);
+			this.Number1Text.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Number1Text.Name = "Number1Text";
-			this.Number1Text.Size = new System.Drawing.Size(118, 25);
+			this.Number1Text.Size = new System.Drawing.Size(90, 21);
 			this.Number1Text.TabIndex = 4;
 			this.Number1Text.Text = "";
 			// 
 			// ResultText
 			// 
-			this.ResultText.Location = new System.Drawing.Point(78, 73);
+			this.ResultText.Location = new System.Drawing.Point(58, 59);
+			this.ResultText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.ResultText.Name = "ResultText";
 			this.ResultText.ReadOnly = true;
-			this.ResultText.Size = new System.Drawing.Size(227, 23);
+			this.ResultText.Size = new System.Drawing.Size(171, 19);
 			this.ResultText.TabIndex = 5;
 			this.ResultText.Text = "";
 			// 
@@ -76,33 +81,47 @@ namespace CshApplication
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Segoe UI", 16F);
-			this.label1.Location = new System.Drawing.Point(165, 34);
+			this.label1.Location = new System.Drawing.Point(124, 28);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(28, 37);
+			this.label1.Size = new System.Drawing.Size(23, 30);
 			this.label1.TabIndex = 6;
-			this.label1.Text = "*";
+			this.label1.Text = "?";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Segoe UI", 16F);
-			this.label2.Location = new System.Drawing.Point(37, 72);
+			this.label2.Location = new System.Drawing.Point(28, 58);
+			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(35, 37);
+			this.label2.Size = new System.Drawing.Size(28, 30);
 			this.label2.TabIndex = 7;
 			this.label2.Text = "=";
 			// 
+			// Add
+			// 
+			this.Add.Location = new System.Drawing.Point(87, 90);
+			this.Add.Name = "Add";
+			this.Add.Size = new System.Drawing.Size(75, 23);
+			this.Add.TabIndex = 8;
+			this.Add.Text = "+";
+			this.Add.UseVisualStyleBackColor = true;
+			this.Add.Click += new System.EventHandler(this.Add_Click);
+			// 
 			// ApplicationGUI
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(332, 140);
+			this.ClientSize = new System.Drawing.Size(249, 114);
+			this.Controls.Add(this.Add);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.ResultText);
 			this.Controls.Add(this.Number1Text);
 			this.Controls.Add(this.Number2Text);
 			this.Controls.Add(this.Multiply);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Name = "ApplicationGUI";
 			this.Text = "ApplicationGUI";
 			this.ResumeLayout(false);
@@ -118,5 +137,6 @@ namespace CshApplication
 		private System.Windows.Forms.RichTextBox ResultText;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button Add;
 	}
 }
--- ApplicationGUI.cs
+++ ApplicationGUI.cs
@@ -1,27 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/****************************************************************\
+* This code written by Itai Danielly                             *
+* This program is free software: you can redistribute it and/or  *
+* modify it under the terms of the GNU General Public License    *
+* as published by the Free Software Foundation, either version 3 *
+* of the License, or (at your option) any later version.         *
+* This program is distributed in the hope that it will be useful *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+* See the GNU General Public License for more details.           *
+\****************************************************************/
+using System;
 using System.Windows.Forms;
+
+
+
+
 
 namespace CshApplication
 {
 	public partial class ApplicationGUI : Form
 	{
+		public const string INPUT_ERROR = "Input error";
+
 		public ApplicationGUI()
 		{
 			InitializeComponent();
 		}
 
+		private bool getNums(ref double num1, ref double num2){
+			bool result = true;
+			try
+			{
+				num1 = Double.Parse(Number1Text.Text);
+				num2 = Double.Parse(Number2Text.Text);
+			}catch(Exception e){
+				result = false;
+			}
+			return result;
+		}
+
 		private void Multiply_Click(object sender, EventArgs e)
 		{
-			int num1 = Int32.Parse(Number2Text.Text);
-			int num2 = Int32.Parse(Number1Text.Text);
-			ResultText.Text = (num1 * num2).ToString();
+			double num1 = 0, num2 = 0;
+			if(getNums(ref num1, ref num2)){
+				ResultText.Text = (num1 * num2).ToString();
+			}
+			else{
+				ResultText.Text = INPUT_ERROR;
+			}
+		}
+
+		private void Add_Click(object sender, EventArgs e)
+		{
+			double num1 = 0, num2 = 0;
+			if (getNums(ref num1, ref num2))
+			{
+				ResultText.Text = (num1 + num2).ToString();
+			}
+			else
+			{
+				ResultText.Text = INPUT_ERROR;
+			}
 		}
 
 #if UNIT_TEST_ENA
@@ -34,6 +73,17 @@ namespace CshApplication
 
 			return ResultText.Text;
 		}
+
+		public string CmdAdd(string num1, string num2)
+		{
+			Number2Text.Text = num1;
+			Number1Text.Text = num2;
+
+			Add_Click(null, null);
+
+			return ResultText.Text;
+		}
+
 #endif
 	}
 }
